7bf84591b01fb1e5d42973dd1131a3008e5bdf8e
maknetaRo <makneta@gmail.com>
2021-05-05 22:07:52 +0200

    added news section

--- src/components/modules/Titles.js
+++ src/components/modules/Titles.js
@@ -7,12 +7,21 @@ export const SectionTitle = styled.h3`
   margin-top: 0;
 `;
 
+export const ArticleTitle = styled.h4`
+  font-family: 'Noto Sans JP', sans-serif;
+  font-size: 1rem;
+  margin-top: 0;
+`;
+
 export const LargeNums = styled.p`
    font-family: 'Noto Sans JP', sans-serif;
    font-size: 1.75rem;
    margin-top: 0;
    padding: 0.25rem 0 0.5rem;
    margin-bottom: 0;
+   @media (max-width: 989px) {
+     font-size: 1.35rem;
+   }
 
    @media (max-width: 699px) {
     font-size: 1.25rem;
