6be61ab2e2f8c72412ec62a69cb3368506c87eef
maknetaRo <makneta@gmail.com>
2021-07-20 21:55:39 +0200

    change the circle marker radius

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,19 @@
-import React, { useRef, useEffect } from 'react';
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  Circle,
-  CircleMarker,
-} from 'react-leaflet';
+import React from 'react';
+import { MapContainer, TileLayer, Popup, CircleMarker } from 'react-leaflet';
 
 const Map = (props) => {
   const center_position = [31.262218, 34.801472];
   const zoom = 1.5;
+
+  const scaleRadius = (cases) => {
+    let number = 6000000;
+    let r = (cases / number) * 10 + 1;
+    console.log(r);
+    if (r < 10) {
+      return 4;
+    }
+    return r;
+  };
 
   return (
     <section className="map" style={{ width: '100%' }}>
@@ -46,29 +49,42 @@ const Map = (props) => {
                   center={coordinates}
                   key={place.properties.country}
                   color={'rgba(26, 115, 232, 0.878)'}
-                  fillColor={'rgba(26, 115, 232, 0.659)'}
-                  fillOpacity={0.5}
+                  fillColor={'rgba(26, 115, 232, 0.878)'}
+                  fillOpacity={0.7}
                   weight={2}
-                  radius={Math.log(cases)}
+                  radius={scaleRadius(cases)}
                 >
                   <Popup>
-                    <img src={flag} style={{ width: '30px', height: 'auto' }} />
-                    <h2>{country}</h2>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <img
+                        src={flag}
+                        style={{ width: '30px', height: 'auto' }}
+                      />
+                      <h2>{country}</h2>
+                    </div>
+
                     <p>
-                      <strong>Cases:</strong> {cases} |{' '}
-                      <strong>Cases Today:</strong> {todayCases}
+                      <strong>Cases:</strong> {cases.toLocaleString()}
+                      {/*<strong>Cases Today:</strong> {todayCases}*/}
                     </p>
                     <p>
-                      <strong>Deaths:</strong> {deaths} |{' '}
-                      <strong>Death Today:</strong> {todayDeaths}
+                      <strong>Deaths:</strong> {deaths.toLocaleString()}
+                      {/*<strong>Death Today:</strong> {todayDeaths}*/}
                     </p>
-                    <p>
+                    {/*<p>
                       <strong>Recovered:</strong> {recovered} |{' '}
                       <strong>Recovered Today:</strong> {todayRecovered}
-                    </p>
-                    <p>
+                   </p>*/}
+                    {/*<p>
                       <strong>Last Update:</strong> {date.toLocaleDateString()}
-                    </p>
+                    </p>*/}
                   </Popup>
                 </CircleMarker>
               );
