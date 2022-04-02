8165d3f5a736cc1d4710821ed406449ec0e4848a
maknetaRo <makneta@gmail.com>
2022-04-03 01:14:57 +0200

    fix data historical

--- src/components/DataHistorical.js
+++ src/components/DataHistorical.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Chart from './Chart'
+import Chart from './Chart';
 import { StyledSection, ChartSection } from './modules/Sections';
 import { SectionTitle } from './modules//Titles';
 import { StyledButton } from './modules/Buttons';
 
 const DataHistorical = ({ dataHistAll }) => {
-  console.log(dataHistAll);
   return (
     <ChartSection>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -16,18 +15,16 @@ const DataHistorical = ({ dataHistAll }) => {
         <>
           {[dataHistAll].map((elem) => {
             const { cases, deaths } = elem[0];
-            console.log(cases);
-            console.log(deaths);
             return (
               <React.Fragment>
                 <StyledSection style={{ marginLeft: '0', marginRight: '0' }}>
                   <SectionTitle>Cases</SectionTitle>
-                  {!cases ? "" :  <Chart data={cases} /> }                 
+                  {!cases ? '' : <Chart data={cases} />}
                 </StyledSection>
                 <StyledSection style={{ marginLeft: '0', marginRight: '0' }}>
                   <SectionTitle>Deaths</SectionTitle>
-                  {!deaths ? "" : <Chart data={deaths} />}
-            </StyledSection>
+                  {!deaths ? '' : <Chart data={deaths} />}
+                </StyledSection>
               </React.Fragment>
             );
           })}
